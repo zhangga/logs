46f2a4c6f30ca28d2604eee8a4b2235a01b5b296
zeqiang.zhang <zeqiang.zhang@funplus.com>
2021-12-21 14:37:03 +0800

    log format bugfix

--- logs.go
+++ logs.go
@@ -41,58 +41,58 @@ func SetLogLevel(level Level) {
 }
 
 func Debug(args ...interface{}) {
-	defLogger.Debug(args)
+	defLogger.Debug(args...)
 }
 func Info(args ...interface{}) {
-	defLogger.Info(args)
+	defLogger.Info(args...)
 }
 func Warn(args ...interface{}) {
-	defLogger.Warn(args)
+	defLogger.Warn(args...)
 }
 func Error(args ...interface{}) {
-	defLogger.Error(args)
+	defLogger.Error(args...)
 }
 func Panic(args ...interface{}) {
-	defLogger.Panic(args)
+	defLogger.Panic(args...)
 }
 func Fatal(args ...interface{}) {
-	defLogger.Fatal(args)
+	defLogger.Fatal(args...)
 }
 
 func Debugf(template string, args ...interface{}) {
-	defLogger.Debugf(template, args)
+	defLogger.Debugf(template, args...)
 }
 func Infof(template string, args ...interface{}) {
-	defLogger.Infof(template, args)
+	defLogger.Infof(template, args...)
 }
 func Warnf(template string, args ...interface{}) {
-	defLogger.Warnf(template, args)
+	defLogger.Warnf(template, args...)
 }
 func Errorf(template string, args ...interface{}) {
-	defLogger.Errorf(template, args)
+	defLogger.Errorf(template, args...)
 }
 func Panicf(template string, args ...interface{}) {
-	defLogger.Panicf(template, args)
+	defLogger.Panicf(template, args...)
 }
 func Fatalf(template string, args ...interface{}) {
-	defLogger.Fatalf(template, args)
+	defLogger.Fatalf(template, args...)
 }
 
 func CtxDebugf(ctx context.Context, template string, args ...interface{}) {
-	defLogger.CtxDebugf(ctx, template, args)
+	defLogger.CtxDebugf(ctx, template, args...)
 }
 func CtxInfof(ctx context.Context, template string, args ...interface{}) {
-	defLogger.CtxInfof(ctx, template, args)
+	defLogger.CtxInfof(ctx, template, args...)
 }
 func CtxWarnf(ctx context.Context, template string, args ...interface{}) {
-	defLogger.CtxWarnf(ctx, template, args)
+	defLogger.CtxWarnf(ctx, template, args...)
 }
 func CtxErrorf(ctx context.Context, template string, args ...interface{}) {
-	defLogger.CtxErrorf(ctx, template, args)
+	defLogger.CtxErrorf(ctx, template, args...)
 }
 func CtxPanicf(ctx context.Context, template string, args ...interface{}) {
-	defLogger.CtxPanicf(ctx, template, args)
+	defLogger.CtxPanicf(ctx, template, args...)
 }
 func CtxFatalf(ctx context.Context, template string, args ...interface{}) {
-	defLogger.CtxFatalf(ctx, template, args)
+	defLogger.CtxFatalf(ctx, template, args...)
 }
